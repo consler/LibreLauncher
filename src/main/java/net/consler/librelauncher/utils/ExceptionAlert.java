a75973e73665154cdb1a2d275af1a6d2a70ec1fa
Consler <consler2000@gmail.com>
2026-09-19 22:30:40 +0200

    improved error handling

--- src/main/java/net/consler/librelauncher/utils/ExceptionAlert.java
+++ src/main/java/net/consler/librelauncher/utils/ExceptionAlert.java
@@ -2,15 +2,22 @@ package net.consler.librelauncher.utils;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import net.consler.librelauncher.exceptions.FailedToLaunchMinecraftException;
 import net.consler.librelauncher.exceptions.InstanceCreationException;
 import net.consler.librelauncher.exceptions.FxmlLoadException;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class ExceptionAlert
 {
     public static void show(Throwable t)
     {
-        Platform.runLater(() -> {
+        Platform.runLater(() ->
+        {
             String title = "Error";
             String header = t.getClass().getSimpleName();
             String content = t.getMessage() == null ? "An unexpected error occurred." : t.getMessage();
@@ -25,10 +32,29 @@ public class ExceptionAlert
                 default -> {}
             }
 
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            t.printStackTrace(printWriter);
+            String errorText = stringWriter.toString();
+
             Alert alert = new Alert(type);
             alert.setTitle(title);
             alert.setHeaderText(header);
             alert.setContentText(content);
+
+            ButtonType copyButton = new ButtonType("Copy Error");
+            alert.getButtonTypes().add(copyButton);
+
+            alert.setOnCloseRequest(event ->
+            {
+                if (alert.getResult() == copyButton)
+                {
+                    ClipboardContent clipboardContent = new ClipboardContent();
+                    clipboardContent.putString(errorText);
+                    Clipboard.getSystemClipboard().setContent(clipboardContent);
+                }
+            });
+
             alert.showAndWait();
         });
     }
